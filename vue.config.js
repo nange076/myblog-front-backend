6a77f776b00829ee9f3d2a00e6400ffbf8df2dab
haonan <1914355916@qq.com>
2022-11-07 13:09:53 +0800

    生产环境https化

--- vue.config.js
+++ vue.config.js
@@ -30,7 +30,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8989`,
+        target: `http://localhost:9001/blog/admin`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
